3ef724fc5d1ccd6c8230d28fd403f23ee4077dfe
fixorafile-arch <fixorafile@gmail.com>
2025-12-14 10:14:29 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://1x3utiqbcbfnpicjaydd.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IjF4M3V0aXFiY2JmbnBpY2pheWRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MjQxMzUsImV4cCI6MjA1MDUwMDEzNX0.PLq1TBpo-r3Ql1xd1jz5ieXet-z31_JPQLTBxp";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error('Supabase URL or Anon Key is missing!');
